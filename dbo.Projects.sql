02049b221c63e071490f8c7dac9cb33810db7e64
pardisghadrdan <pardisghadrdan@gmail.com>
2018-03-07 15:21:46 +0100

    färdig

---
+++ dbo.Projects.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Projects] (
+    [Id]       INT           IDENTITY (1, 1) NOT NULL,
+    [Name]     NVARCHAR (50) NOT NULL,
+    [Category] NVARCHAR (50) NOT NULL,
+    [Hours]    INT           NOT NULL,
+    [Minutes]  INT           NOT NULL,
+    [Seconds]  INT           NOT NULL,
+    [Hundreds ] INT NOT NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
